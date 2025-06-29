2fe34e80b85e7f4363ee07beb145134f96027604
Cong Le <longchike@gmail.com>
2025-06-29 02:28:50 -0700

    The setup

---
+++ Recapitulation/RecapitulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapitulationApp.swift
+//  Recapitulation
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RecapitulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
